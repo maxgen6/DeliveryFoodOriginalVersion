a69d7ab570f6b24eb155362d0cd47e3aa1814f10
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-06-18 17:57:23 +0300

    запросы

--- jsFile.js
+++ jsFile.js
@@ -1,1 +1,66 @@
-console.log(window.innerWidth)
+const sendForm = () => {
+    const errorMessage = 'Что-то пошло не так',
+        loadMessage = 'Загрузка...',
+        succesMessage = 'Спасибо! Мы скоро с вами свяжемся!';
+
+    const form = document.querySelector('.form'),
+        statusMessage = document.createElement('div');
+    
+    statusMessage.style.cssText = 'font-size: 2rem; color: black;';
+
+    const validate = () => {
+        let formName = document.querySelector('.form-name'),
+            formMail = document.querySelector ('.form-email'),
+            formPhone = document.querySelector('.form-phone');
+        
+        formName.addEventListener('input', () => {
+            formName.value = formName.value.replace(/\w/gi, '');
+        });
+        formMail.addEventListener('input', () => {
+            formMail.value = formMail.value.replace(/\w/gi, '');
+        });
+        formPhone.addEventListener('input', () => {
+            formPhone.value = formPhone.value.replace(/\D/g,'');
+        });
+    };
+    // validate();
+     
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+        statusMessage.textContent  = loadMessage;
+
+        const formData = new FormData(form);
+
+        let body = {};
+
+        formData.forEach((val, key) => {
+            body[key] = val;
+        });
+        postData(body)
+            .then((response) => {
+                if(response.status === 405){
+                    throw new Error('status network not 200');
+                }
+                statusMessage.textContent = succesMessage;
+            })
+            .catch((error) => {
+                statusMessage.textContent = errorMessage;
+                console.log(error);
+            });
+    });
+
+    const postData = (body) => {
+        return fetch('./server.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body),
+            credentials: 'include'
+        });
+    }
+
+
+};
+sendForm();
